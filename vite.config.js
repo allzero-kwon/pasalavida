296f604932e429980d2f4fdbdabb119214f887ce
Dayoung Kwon <da02@Dayoung-Mac.local>
2024-08-11 17:08:39 +0900

    Update code

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/pasalavida/",
     plugins: [react(),
         withReactRouter(),
         tsconfigPaths(),
